6efff1578b96243ece0126a712a086943672cad5
GonzalezAndrew <andrewgonzo23@gmail.com>
2022-11-30 17:30:38 -0600

    chaging example

--- main.tf
+++ main.tf
@@ -2,5 +2,11 @@
 # Provider
 #------------------------------------------------------------------------------
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
+  alias  = "use1"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  alias  = "euw1"
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,6 @@
 #------------------------------------------------------------------------------
 # Variables
 #------------------------------------------------------------------------------
-variable "region" {
-  description = "The AWS region."
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "tags" {
   description = "A map of tags to add to all resources."
   type        = map(string)
